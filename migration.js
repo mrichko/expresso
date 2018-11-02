b35103c3a6fd0d467e2d0a268ec590a2b5145c08
Mike Richko <mrichko@icloud.com>
2018-11-02 14:26:27 -0500

    adjusted my SQL table formatting

--- migration.js
+++ migration.js
@@ -5,37 +5,36 @@ const db = new sqlite3.Database('./database.sqlite');
 db.serialize(() => {
     db.run('DROP TABLE IF EXISTS Employee');
     db.run(`CREATE TABLE Employee (
-          id INTEGER  REQUIRED,
-          name TEXT REQUIRED,
-          position TEXT REQUIRED,
-          wage INTEGER REQUIRED,
+          id INTEGER  ,
+          name TEXT NOT NULL,
+          position TEXT NOT NULL,
+          wage INTEGER NOT NULL,
           is_current_employee INTEGER DEFAULT 1,
           PRIMARY KEY (id)
-    );`, if err => {
+    )`, (error) => {
+      if (error){
       console.log('There was an error when creating the Employee table')
-    } else {
-      console.log('The Employee database was created succesfully!')
     }
-
-});
+  });
 
 //Creating timesheet table
 db.run('DROP TABLE IF EXISTS Timesheet');
 db.run(`CREATE TABLE Timesheet (
-      id INTEGER REQUIRED,
-      hours INTEGER REQUIRED,
-      rate INTEGER REQUIRED,
-      date INTEGER REQUIRED,
-      PRIMARY KEY (id),
-      FOREIGN KEY(employee_id) REFERENCES Employee(id)
-);`);
+  id INTEGER NOT NULL,
+  hours INTEGER NOT NULL,
+  rate INTEGER NOT NULL,
+  date INTEGERN NOT NULL,
+  employee_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(employee_id) REFERENCES Employee(id)
+)`);
 
 
 // Creating menu table
 db.run('DROP TABLE IF EXISTS Menu');
 db.run(`CREATE TABLE Menu (
-      id INTEGER REQUIRED,
-      title TEXT REQUIRED,
+      id INTEGER NOT NULL,
+      title TEXT NOT NULL,
       PRIMARY KEY (id)
 );`);
 
